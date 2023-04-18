e3b16d60c7cdde95e6dfdc297e392cf9f9eb4cf8
manhhv87 <manhhv87@gmail.com>
2023-04-18 17:53:10 +0700

    modify file code

--- OD/models/Yolov7/ConvertCocoJsonToYoloFormat.py
+++ OD/models/Yolov7/ConvertCocoJsonToYoloFormat.py
@@ -23,7 +23,7 @@ def computeInput(annotationPath, outputPath):
                 w_img = image['width']
                 yoloFormat = convertToYolo(
                     key['bbox'][0], key['bbox'][1], key['bbox'][2], key['bbox'][3], w_img, h_img)
-                f = open(outputPath + "\\" +
+                f = open(outputPath +
                          str(key['image_id']) + ".txt", "a")
                 f.write(str(key['category_id']) + " " + str(yoloFormat[0]) + " " + str(
                     yoloFormat[1]) + " " + str(yoloFormat[2]) + " " + str(yoloFormat[3]) + "\n")
@@ -37,24 +37,24 @@ parser.add_argument('--annotations', type=str, default=None,
                     help='path to the folder, which contains the annotation json files')
 parser.add_argument('--labels', type=str, default=None,
                     help='path to yolov7-seadronesee/data folder')
-parser.add_argument('--val', action = 'store_true',
-                    help = 'convert the val annotation')
-parser.add_argument('--train', action = 'store_true',
-                    help = 'convert the train annotation')
-parser.add_argument('--test', action = 'store_true',
-                    help = 'convert the test annotation')
-options=parser.parse_args()
+parser.add_argument('--val', action='store_true',
+                    help='convert the val annotation')
+parser.add_argument('--train', action='store_true',
+                    help='convert the train annotation')
+# parser.add_argument('--test', action='store_true',
+#                     help='convert the test annotation')
+options = parser.parse_args()
 
 if (options.val == None and options.train == None and options.test == None):
     print("Please specify the annotations to convert")
     exit()
 if (options.val):
-    computeInput(options.annotations + "\instances_val.json",
-                 options.labels + "\\val\labels")
+    computeInput(options.annotations + "instances_val.json",
+                 options.labels + "val/labels/")
 if (options.train):
-    computeInput(options.annotations + "\instances_train.json",
-                 options.labels + "\\train\labels")
-if (options.test):
-    computeInput(options.annotations + "\instances_test.json",
-                 options.labels + "\\test\labels")
+    computeInput(options.annotations + "instances_train.json",
+                 options.labels + "train/labels/")
+# if (options.test):
+#     computeInput(options.annotations + "instances_test.json",
+#                  options.labels + "test/labels/")
 print("Done")
